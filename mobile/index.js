51ebd50dad47c827dd36b2a8dbe1dcc6978ea054
Amirul Faqih <fakiiahmad001@gmail.com>
2024-07-05 23:54:48 +0300

    Refactor AppRegistry.registerComponent to include the App component as the main component

--- mobile/index.js
+++ mobile/index.js
@@ -3,4 +3,5 @@ import 'react-native-gesture-handler';
 import App from './App';
 import { name as appName } from './app.json';
 
+// Register the App component as the main component
 AppRegistry.registerComponent(appName, () => App);
